4b1df74cb4efa86a8d68b6804fc46e43770a7f7c
SardorYusupaliev <sardor.yusupaliev02@gmail.com>
2026-02-16 12:50:06 +0500

    Add files via upload

---
+++ homework-3/build/CMakeFiles/homework_3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework_3.dir/main.cpp.o"
+  "CMakeFiles/homework_3.dir/main.cpp.o.d"
+  "homework_3"
+  "homework_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
